f1b3d64dab76161c3af90e20922addd587492119
Fitri Dhsia <fiturbra438@gmail.com>
2026-08-27 03:02:52 +0800

    4

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,9 @@ mod handlers;
 
 #[tokio::main]
 async fn main() {
-    // 1. URL Koneksi ke PostgreSQL yang ada di Docker kamu
-    let database_url = "postgres://admin:admin1206@localhost:5432/TOURNAMEN_ESPORT";
+    let database_url: &str = "postgres://admin:admin1206@localhost:5432/TOURNAMEN_ESPORT";
 
-    // 2. Membuka koneksi pool ke database
-    let pool = PgPoolOptions::new()
+    let pool: sqlx::Pool<sqlx::Postgres> = PgPoolOptions::new()
         .max_connections(5)
         .connect(database_url)
         .await
@@ -17,13 +15,11 @@ async fn main() {
 
     println!("✅ Berhasil terhubung ke database TOURNAMEN_ESPORT!");
 
-    // 3. Menyiapkan rute API dasar
     let app = Router::new()
         .route("/", get(|| async { "API Turnamen E-Sport Berjalan Mantap!" }))
         .route("/api/users", axum::routing::post(handlers::register_user))  
         .with_state(pool);
 
-    // 4. Menyalakan server di port 8080
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
     println!("🚀 Server siap menerima request di http://127.0.0.1:8080");
     
